dc95fb8b3291aeddd4701196772b586da0c18baa
Joseph Bloom <jsphbloom@gmail.com>
2024-12-13 08:48:15 -0700

    adding plate type to vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -7,6 +7,7 @@ class Vehicle
               :model,
               :engine,
               :registration_date
+              :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,7 +15,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
-    
+    @plate_type = vehicle_details[:plate_type]
   end
 
   def antique?
