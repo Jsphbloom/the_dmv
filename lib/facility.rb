4f7b7796abb5d38b91c0345d5237c57850ff9eb5
Joseph Bloom <jsphbloom@gmail.com>
2024-12-13 08:39:48 -0700

    register vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -7,10 +7,24 @@ class Facility
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
 
     @services << service
   end
+
+  def register_vehicle(vehicle)
+      if vehicle.antique?
+        @collected_fees += 25
+        @registered_vehicles << vehicle
+      elsif vehicle.engine == 'ev'
+        @collected_fees += 200
+        @registered_vehicles << vehicle
+      else
+        @collected_fees += 100
+        @registered_vehicles << vehicle
+      end
 end
