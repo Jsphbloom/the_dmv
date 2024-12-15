3df417d96e486c9478f030e7179ab5767b5f98ae
Joseph Bloom <jsphbloom@gmail.com>
2024-12-15 13:00:11 -0700

    forgot to require the vehiclefactory file, it can initialize now

--- spec/vehiclefactory_spec.rb
+++ spec/vehiclefactory_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+require './lib/vehiclefactory'
 
 RSpec.describe Vehicle_factory do
-    @vehicle_factory = Vehicle_factory.new(DmvDataService.new.wa_ev_registrations)  
+    vehicle_factory = Vehicle_factory.new(DmvDataService.new.wa_ev_registrations)  
     describe "#initialize" do
     it 'can initialize' do
-        expect(@vehicle_factory).to be_an_instance_of(Vehicle_factory)
+        expect(vehicle_factory).to be_an_instance_of(Vehicle_factory)
         end
     end
 end
