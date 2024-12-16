b2cd5892bc40a50eea9b3d652927699c43fb9429
Joseph Bloom <jsphbloom@gmail.com>
2024-12-16 12:16:38 -0700

    messing with create vehicles

--- lib/vehiclefactory.rb
+++ lib/vehiclefactory.rb
@@ -8,10 +8,18 @@ attr_accessor :cars
         @cars = []
     end
     
-    def create_vehicles(registrations)
-        registrations.each do |registration|
-            @cars << Vehicle.new(registration)
+    def create_vehicles(vehicle_details)
+        vehicle_details.each do |vehicle|
+            new_vehicle = Vehicle.new({
+            vehicle_details[:vin],
+            vehicle_details[:year],
+            vehicle_details[:make],
+            vehicle_details[:model],
+            vehicle_details[:engine] = :ev
+            })
+            if new_vehicle.model == "Prius Plug-in"
+                @cars << new_vehicle
+            end
         end
-        return @cars
     end
 end
--- spec/vehiclefactory_spec.rb
+++ spec/vehiclefactory_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 require './lib/vehiclefactory'
 
 RSpec.describe Vehicle_factory do
+    before do
+        @prius = [{:vin_1_10=>"JTDKN3DP8D",
+        :dol_vehicle_id=>"229686908",
+        :model_year=>"2013",
+        :make=>"TOYOTA",
+        :model=>"Prius Plug-in"}]
+    end
+
     describe "#initialize" do
         it 'can initialize' do
             vehicle_factory = Vehicle_factory.new 
@@ -15,7 +23,7 @@ RSpec.describe Vehicle_factory do
             factory = Vehicle_factory.new
             wa_ev_registrations = DmvDataService.new.wa_ev_registrations
             factory.create_vehicles(wa_ev_registrations)
-            expect(factory.cars).to eq([wa_ev_registrations])
+            expect(cars[0]).to eq(["JTDKN3DP8D"])
         end
     end
 end
