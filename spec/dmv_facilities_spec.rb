73efab84dbf7f8584ffb99520f093f9a6332fa57
Joseph Bloom <jsphbloom@gmail.com>
2024-12-17 14:48:13 -0700

    spec tests looking nice on dmv_facilities_spec

--- spec/dmv_facilities_spec.rb
+++ spec/dmv_facilities_spec.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 RSpec.describe DmvFacilities do
     before do
-        @dmvfacility = DmvFacilities.new
+        @dmvfacilities = DmvFacilities.new
         @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
         @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
         @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
@@ -11,59 +11,55 @@ RSpec.describe DmvFacilities do
     
     describe "#initialize" do
         it 'can initialize' do
-            expect(@dmvfacility).to be_an_instance_of(DmvFacilities)
+            expect(@dmvfacilities).to be_an_instance_of(DmvFacilities)
         end
     end
 
     describe "#create CO facilities" do
-        it 'can create CO facilities and populate with facility details' do
-            @dmvfacility.create_facilities(@co_dmv_office_locations)
-            expect(@dmvfacility.facilities.length).to eq(5)
-        end
-    end
-
-    describe "#check specific CO facility in array" do
-        it "check office of 2nd facility" do
-            @dmvfacility.create_facilities(@co_dmv_office_locations)
-            expect(@dmvfacility.facilities[1].name).to eq("DMV Northeast Branch")
+        it 'can create CO facilities and hold facility details' do
+            @dmvfacilities.create_facilities(@co_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(5)
+            expect(@dmvfacilities.facilities[1].name).to eq("DMV Northeast Branch")
+            expect(@dmvfacilities.facilities[1].phone).to eq("(720) 865-4600")
+            expect(@dmvfacilities.facilities[1].address).to eq("4685 Peoria Street")
         end
     end
 
     describe "#create NY facilities" do
-        it 'can create NY facilities and populate with facility details' do
-            @dmvfacility.create_facilities(@ny_dmv_office_locations)
-            expect(@dmvfacility.facilities.length).to eq(173)
-        end
-    end
-
-    describe "#check specific NY facility in array" do
-        it "check office of 2nd facility" do
-            @dmvfacility.create_facilities(@ny_dmv_office_locations)
-            expect(@dmvfacility.facilities[1].name).to eq("HUDSON")
+        it 'can create NY facilities and hold facility details' do
+            @dmvfacilities.create_facilities(@ny_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(173)
+            expect(@dmvfacilities.facilities[0].name).to eq("LAKE PLACID")
+            expect(@dmvfacilities.facilities[1].name).to eq("HUDSON")
+            expect(@dmvfacilities.facilities[2].name).to eq("RIVERHEAD KIOSK")
         end
     end
 
     describe "#create MO facilities" do
-        it 'can create MO facilities and populate with facility details' do
-            @dmvfacility.create_facilities(@mo_dmv_office_locations)
-            expect(@dmvfacility.facilities.length).to eq(174)
-        end
-    end
-
-    describe "#check specific MO facility in array" do
-        it "check office of 2nd facility" do
-            @dmvfacility.create_facilities(@mo_dmv_office_locations)
-            expect(@dmvfacility.facilities[1].name).to eq("High Ridge")
+        it 'can create MO facilities and hold facility details' do
+            @dmvfacilities.create_facilities(@mo_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(174)
+            expect(@dmvfacilities.facilities[1].name).to eq("High Ridge")
+            expect(@dmvfacilities.facilities[0].address).to eq("2009 Plaza Dr.")
+            expect(@dmvfacilities.facilities[2].phone).to eq("(660) 665-0292")
         end
     end
 
     describe "#adds all facilities" do
         it "can add facilities from all states at once" do
-            @dmvfacility.create_facilities(@mo_dmv_office_locations)
-            @dmvfacility.create_facilities(@ny_dmv_office_locations)
-            @dmvfacility.create_facilities(@co_dmv_office_locations)
+            mo_count = @mo_dmv_office_locations.count
+            ny_count = @ny_dmv_office_locations.count
+            co_count = @co_dmv_office_locations.count
 
-            expect(@dmvfacility.facilities.length).to eq(352)
+            @dmvfacilities.create_facilities(@mo_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(mo_count)
+
+            @dmvfacilities.create_facilities(@ny_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(mo_count + ny_count)
+            
+            @dmvfacilities.create_facilities(@co_dmv_office_locations)
+            expect(@dmvfacilities.facilities.length).to eq(mo_count + ny_count + co_count)
+
         end
     end
 end
